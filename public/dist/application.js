cb230af1e00a93289d7c4b92ced990e8f709596b
Jared Q <jaredqin@gmail.com>
2015-04-23 12:22:27 +1000

    remove some playIt clicks

--- public/dist/application.js
+++ public/dist/application.js
@@ -453,15 +453,7 @@ angular.module('local').config(['$stateProvider',
 /*global MediaElement, $*/
 
 angular.module('local').controller('LocalController', ['$scope',
-	function($scope) {		
-		
-		$scope.start = function() {
-			$('video').mediaelementplayer({
-				success: function(media, node, player) {
-					$('#' + node.id + '-mode').html('mode: ' + media.pluginType);
-				}
-			});
-		};
+	function($scope) {
 	}
 ]);
 'use strict';
--- public/modules/local/controllers/local.client.controller.js
+++ public/modules/local/controllers/local.client.controller.js
@@ -2,14 +2,6 @@
 /*global MediaElement, $*/
 
 angular.module('local').controller('LocalController', ['$scope',
-	function($scope) {		
-		
-		$scope.start = function() {
-			$('video').mediaelementplayer({
-				success: function(media, node, player) {
-					$('#' + node.id + '-mode').html('mode: ' + media.pluginType);
-				}
-			});
-		};
+	function($scope) {
 	}
 ]);
